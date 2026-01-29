eb8ae1f7074a202f455ab1701256ca794632285d
Nikolay Mitrofanov <30695496+name212@users.noreply.github.com>
2026-01-29 21:25:36 +0300

    call parent process logger in InMemoryLogger (#21)
    Signed-off-by: Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>

--- pkg/log/in_memory_test.go
+++ pkg/log/in_memory_test.go
@@ -14,7 +14,11 @@
 
 package log
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestInMemoryLoggerFollowInterfaces(t *testing.T) {
 	t.Run("Default constructor", func(t *testing.T) {
@@ -23,5 +27,50 @@ func TestInMemoryLoggerFollowInterfaces(t *testing.T) {
 
 	t.Run("With parent constructor", func(t *testing.T) {
 		assertFollowAllInterfaces(t, NewInMemoryLoggerWithParent(NewDummyLogger(true)))
+	})
+}
+
+func TestInMemoryLoggerProcessLogger(t *testing.T) {
+	assertAll := func(t *testing.T, logger *InMemoryLogger, expected []string) {
+		for _, prefix := range expected {
+			matches, err := logger.AllMatches(&Match{
+				Prefix: []string{prefix},
+			})
+
+			require.NoError(t, err)
+			require.Len(t, matches, 1, "should only have one match for %s", prefix)
+			require.Equal(t, prefix, matches[0])
+		}
+	}
+
+	t.Run("Success", func(t *testing.T) {
+		logger := NewInMemoryLoggerWithParent(NewPrettyLogger(LoggerOptions{IsDebug: true}))
+		processLogger := logger.ProcessLogger()
+
+		processLogger.ProcessStart("My process")
+		logger.InfoF("Do something")
+		processLogger.ProcessEnd()
+
+		assertAll(t, logger, []string{
+			"Start process: My process",
+			"Do something\n",
+			"End process",
+		})
+	})
+
+	t.Run("Failed", func(t *testing.T) {
+		logger := NewInMemoryLoggerWithParent(NewPrettyLogger(LoggerOptions{IsDebug: true})).
+			WithErrorPrefix("Error")
+		processLogger := logger.ProcessLogger()
+
+		processLogger.ProcessStart("My failed process")
+		logger.WarnF("Error!")
+		processLogger.ProcessFail()
+
+		assertAll(t, logger, []string{
+			"Start process: My failed process",
+			"Error!\n",
+			"Error: Fail process",
+		})
 	})
 }
